c048e3abb4166ca07b529f64d6becda7448680b5
Aleksandar <41162669+Aca84@users.noreply.github.com>
2021-05-01 09:24:54 +0200

    Security update
    Update Laravel

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,7 +62,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        User::create([
+        //User::create([
+           // 'name' => $data['name'],
+           // 'email' => $data['email'],
+           // 'password' => Hash::make($data['password']),
+           // 'role' => 'user',
+        //]); trow an error 
+		
+		return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
